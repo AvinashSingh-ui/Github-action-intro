f03b2616be836a99e14dfc585d0316ddb74903b4
Avinashsingh-ui <avinashsingh60005@gmail.com>
2026-09-21 17:22:17 +0530

    fix: resolve sum function conflict

--- sum.js
+++ sum.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const sum = (a, b) =>{ a + b
-};
-=======
 const sum = (a, b) => {
-    return a+b;
-}
->>>>>>> 8845e91 (test: updates some bit of code)
+    return a + b;
+};
 
 module.exports = sum;
